b1883c6882b4fb4c9277caf046fef8969a3d4dc2
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2024-12-06 06:31:57 +0200

    Create DatabaseOperations.java

---
+++ Spring_Core/practice-BeansLifecycle/src/DatabaseOperations.java
@@ -0,0 +1,10 @@
+public class DatabaseOperations {
+
+    public void connectToDatabase() {
+        System.out.println("Connection to database established ......");
+    }
+
+    public void disconnectFromDatabase() {
+        System.out.println("Connection to database closed ......");
+    }
+}
