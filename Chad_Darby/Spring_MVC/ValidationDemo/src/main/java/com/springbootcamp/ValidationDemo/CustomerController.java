db460e26245aa7e49772d1dca00574bbd21cde89
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2024-12-22 23:59:39 +0200

    Update CustomerController.java

--- Chad_Darby/Spring_MVC/ValidationDemo/src/main/java/com/springbootcamp/ValidationDemo/CustomerController.java
+++ Chad_Darby/Spring_MVC/ValidationDemo/src/main/java/com/springbootcamp/ValidationDemo/CustomerController.java
@@ -1,15 +1,30 @@
 package com.springbootcamp.ValidationDemo;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class CustomerController {
 
-public String showForm(Model theModel){
+    @GetMapping("/")
+    public String showForm(Model theModel){
+        theModel.addAttribute("customer",new Customer());
+        return "customer-form";
+    }
 
-    theModel.addAttribute("customer",new Customer());
-    return "customer-form";
-}
+    public String processF0rm(@Valid @ModelAttribute("customer") Customer theCustomer,
+                              BindingResult theBindingResult) {
+        if(theBindingResult.hasErrors()){
+            return "customer-form";
+        }else{
+            return "customer-confirmation";
+        }
+
+    }
+
 
 }
