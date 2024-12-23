9e495d964124583784237a984b441a1b608ca7db
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2024-12-23 19:45:06 +0200

    Create employeemvccrud.java

---
+++ Chad_Darby/Spring_MVC_CRUD/employees-mvc-crud/src/main/java/com/springbootcamp/employeemvccrud/employeemvccrud.java
@@ -0,0 +1,13 @@
+package com.springbootcamp.employeemvccrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class employeemvccrud {
+
+	public static void main(String[] args) {
+		SpringApplication.run(employeemvccrud.class, args);
+	}
+
+}
