a90e9b3d409895aeccaf5dce27aa8a435522b096
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2024-12-24 00:26:10 +0200

    Update EmployeeRepository.java

--- Chad_Darby/Spring_MVC_CRUD/employees-mvc-crud/src/main/java/com/springbootcamp/employeemvccrud/dao/EmployeeRepository.java
+++ Chad_Darby/Spring_MVC_CRUD/employees-mvc-crud/src/main/java/com/springbootcamp/employeemvccrud/dao/EmployeeRepository.java
@@ -3,8 +3,10 @@ package com.springbootcamp.employeemvccrud.dao;
 import com.springbootcamp.employeemvccrud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
-    // that's it ... no need to write any code LOL!
+    public List<Employee> findAllByOrderByLastNameAsc();
 
 }
