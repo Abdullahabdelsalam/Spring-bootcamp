b482ec4264bf7eb21206428064a1b048bba1212d
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2024-12-23 03:59:35 +0200

    Update CustomerController.java

--- Chad_Darby/Spring_MVC/ValidationDemo/src/main/java/com/springbootcamp/ValidationDemo/CustomerController.java
+++ Chad_Darby/Spring_MVC/ValidationDemo/src/main/java/com/springbootcamp/ValidationDemo/CustomerController.java
@@ -1,15 +1,24 @@
 package com.springbootcamp.ValidationDemo;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    public void initBinder(WebDataBinder dateBinder){
+
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dateBinder.registerCustomEditor(String.class,stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String showForm(Model theModel){
