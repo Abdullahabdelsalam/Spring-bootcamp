e9a027c9a8ca1c0d869461e0a4e9acf126970283
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2024-12-23 19:45:32 +0200

    Create employeemvccrudTests.java

---
+++ Chad_Darby/Spring_MVC_CRUD/employees-mvc-crud/src/test/java/com/springbootcamp/employeemvccrud/employeemvccrudTests.java
@@ -0,0 +1,13 @@
+package com.springbootcamp.employeemvccrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class employeemvccrudTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
