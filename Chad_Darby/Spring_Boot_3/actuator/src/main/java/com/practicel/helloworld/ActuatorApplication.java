accb68b285492a1fb9f48d4a41c5687419ad9ec1
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2024-12-26 04:20:44 +0200

    Create ActuatorApplication.java

---
+++ Chad_Darby/Spring_Boot_3/actuator/src/main/java/com/practicel/helloworld/ActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.practicel.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorApplication.class, args);
+	}
+
+}
