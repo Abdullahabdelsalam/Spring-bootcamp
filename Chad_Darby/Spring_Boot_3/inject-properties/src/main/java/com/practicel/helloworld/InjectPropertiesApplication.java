2969277e59271ef7449e149870a962c35438aa0d
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2024-12-26 04:19:36 +0200

    Create InjectPropertiesApplication.java

---
+++ Chad_Darby/Spring_Boot_3/inject-properties/src/main/java/com/practicel/helloworld/InjectPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.practicel.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InjectPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InjectPropertiesApplication.class, args);
+	}
+
+}
