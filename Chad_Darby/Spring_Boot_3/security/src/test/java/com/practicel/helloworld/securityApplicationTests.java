3b12cf9f7f01f1ec1bd945f3354bbdb5d4ee9d95
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2024-12-26 04:20:26 +0200

    Create securityApplicationTests.java

---
+++ Chad_Darby/Spring_Boot_3/security/src/test/java/com/practicel/helloworld/securityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicel.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class securityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
