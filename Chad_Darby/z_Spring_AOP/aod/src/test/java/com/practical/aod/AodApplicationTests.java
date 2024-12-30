af57151eb6ef97e0cdb57eeb8cd84fa167c4bfd7
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2024-12-30 21:34:45 +0200

    Create AodApplicationTests.java

---
+++ Chad_Darby/z_Spring_AOP/aod/src/test/java/com/practical/aod/AodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practical.aod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
