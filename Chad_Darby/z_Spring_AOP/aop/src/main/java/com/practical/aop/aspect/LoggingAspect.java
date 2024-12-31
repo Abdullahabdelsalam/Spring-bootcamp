4f96fbe666ed404a2580b4fef7ae1e6dd0c7f3d8
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2024-12-31 05:33:44 +0200

    Update LoggingAspect.java

--- Chad_Darby/z_Spring_AOP/aop/src/main/java/com/practical/aop/aspect/LoggingAspect.java
+++ Chad_Darby/z_Spring_AOP/aop/src/main/java/com/practical/aop/aspect/LoggingAspect.java
@@ -2,6 +2,7 @@ package com.practical.aop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -20,6 +21,7 @@ public class LoggingAspect {
 //
 //        System.out.println("\n====>>>>>Executing @Before advice on addAccount()");
 //    }
+    
 
 //    @Before("execution(public void add*())")
 //    public void beforeAddAccountAdvice() {
@@ -27,11 +29,13 @@ public class LoggingAspect {
 //        System.out.println("\n====>>>>>Executing @Before advice on addAccount()");
 //    }
 
+
 //    @Before("execution( void add*())")
 //    public void beforeAddAccountAdvice() {
 //
 //        System.out.println("\n====>>>>>Executing @Before advice on addAccount()");
 //    }
+
 
 //    @Before("execution(* add*())")
 //    public void beforeAddAccountAdvice() {
@@ -40,11 +44,13 @@ public class LoggingAspect {
 //    }
 
 
+
 //    @Before("execution(* add*(com.practical.aop.Account))")
 //    public void beforeAddAccountAdvice() {
 //
 //        System.out.println("\n====>>>>>Executing @Before advice on addAccount()");
 //    }
+
 
 //    @Before("execution(* add*(com.practical.aop.Account,..))")
 //    public void beforeAddAccountAdvice() {
@@ -52,12 +58,52 @@ public class LoggingAspect {
 //        System.out.println("\n====>>>>>Executing @Before advice on addAccount()");
 //    }
 
-    @Before("execution(* add*(..))")
-    public void beforeAddAccountAdvice() {
 
-        System.out.println("\n====>>>>>Executing @Before advice on addAccount()");
+//    @Before("execution(* add*(..))")
+//    public void beforeAddAccountAdvice() {
+//
+//        System.out.println("\n====>>>>>Executing @Before advice on addAccount()");
+//    }
+
+
+//    @Pointcut("execution(* com.practical.aop.dao.*.*(..))")
+//    private void forDaoPackage() {}
+//
+//    @Before("forDaoPackage()")
+//    public void beforeAddAccountAdvice() {
+//
+//        System.out.println("\n=====>>> Executing @Before advice on method");
+//
+//    }
+//    @Before("forDaoPackage()")
+//    public void performApiAnalytics() {
+//        System.out.println("\n=====>>> Performing API analytics");
+//    }
+
+
+    @Pointcut("execution(* com.practical.aop.dao.*.*(..))")
+    private void forDaoPackage() {}
+
+    // create a pointcut for getter methods
+    @Pointcut("execution(* com.practical.aop.dao.*.get*(..))")
+    private void getter() {}
+
+    // create a pointcut for setter methods
+    @Pointcut("execution(* com.practical.aop.dao.*.set*(..))")
+    private void setter() {}
+
+    // create pointcut: include package ... exclude getter/setter
+    @Pointcut("forDaoPackage() && !(getter() || setter())")
+    private void forDaoPackageNoGetterSetter() {}
+
+    @Before("forDaoPackageNoGetterSetter()")
+    public void beforeAddAccountAdvice() {
+        System.out.println("\n=====>>> Executing @Before advice on method");
     }
 
-
+    @Before("forDaoPackageNoGetterSetter()")
+    public void performApiAnalytics() {
+        System.out.println("\n=====>>> Performing API analytics");
+    }
 
 }
