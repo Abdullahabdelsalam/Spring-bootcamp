dc90a89b6994fcd72aabb5fcc9ab0708951a4097
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2025-01-23 12:32:17 +0200

    Create Player.java

---
+++ Spring_jpa_Hibernate/player/src/main/java/com/traineeship/player/Player.java
@@ -0,0 +1,71 @@
+package com.traineeship.player;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+@Entity
+@Table(name = "Players")
+public class Player {
+
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    public Player(int id, String email, String lastName, String firstName) {
+        this.id = id;
+        this.email = email;
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
